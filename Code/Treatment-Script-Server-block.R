f23f50e03cdf9002570516fa9f0e78dddc68edb2
debsuri <debsuri@debian.gnu-suri>
2021-04-23 12:27:42 -0400

    update code of randomization

--- Code/Treatment-Script-Server-block.R
+++ Code/Treatment-Script-Server-block.R
@@ -4,10 +4,8 @@
 ### Denise Laroze / Francisco Villarroel
 ################################################
 
-args <- commandArgs(TRUE)
-
 #No incluir el llamado a librerias individuales, sino la carpeta donde estÃÂÃÂ¡n instaladas
-.libPaths=("/usr/lib64/R/library/")
+#.libPaths=("/usr/lib64/R/library/")
 
 require(plyr)
 #require(ggplot2)
@@ -29,66 +27,49 @@ require(purrr, warn.conflicts = FALSE, quietly = TRUE)
 
 #Amazon Server
 #setwd("/var/www/r.cess.cl/public_html/")
-setwd("~/GitHub/Master_Thesis/Code")
+setwd("~/Documentos/Git/Master_Thesis/Code")
+
 
 #Parameters
-path<-"/var/www/r.cess.cl/public_html/sp/"
+#path<-"/var/www/r.cess.cl/public_html/sp/"
 
 ####################################################################
 ###########################block randmisation####################### 
 
 #Editar al final cuando tengamos claridad de numero de argumentos
-if(args[6] == "reset_database"){
-  time <- Sys.time()
-  time <- gsub("[:alph:]", "", time)
-  time <- gsub(" ", "_", time)
+#if(args[22] == "reset_database"){
+#  time <- Sys.time()
+#  time <- gsub("[:alph:]", "", time)
+#  time <- gsub(" ", "_", time)
   
-  file.copy("/var/www/r.cess.cl/public_html/sp/new.RData", sprintf("rdata_bak_%s.Rdata", time))
-  file.copy("/var/www/r.cess.cl/public_html/sp/new_orig.RData", "new.RData", overwrite = T)
-  stop()
-}
+#  file.copy("/var/www/r.cess.cl/public_html/sp/new.RData", sprintf("rdata_bak_%s.Rdata", time))
+#  file.copy("/var/www/r.cess.cl/public_html/sp/new_orig.RData", "new.RData", overwrite = T)
+#  stop()
+#}
 
 #args <- as.vector(t(sim.data[i, ]))
-if(length(args) != 3){
-  stop()
-}
+#if(length(args) != 22){
+#  stop()
+#}
 
 # Load data
 #load(file="/var/www/r.cess.cl/public_html/sp/new.RData")
 load(file="new.RData")
 
+#argumentos
+
+#args <- c("QID35","4","6","2","3","2","10","8","7","7","4","6","3","7","4","7","7","7","5","5","6","7","7","5","4")
+
+#args <-c("QI35","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1")
+#args <-c("QI22","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1")
+args <-c("QI44","9","8","9","8","5","6","10","5","5","3","6","7","7","7","7","4","5","1","4","5","6","5","7","7")
+
+
+#ID
+
 QID = args[1]
 
 #### Data Management
-
-#Homofilia
-
-args<-c("Xvar1", "Xvar2", "Xvar3", "Xvar4", "Xvar5", "Xvar6", "Xvar7") #valor observado de todos 
-
-#valor observado de cada item
-digital1<-args[1]  
-digital2<-args[2]  
-digital3<-args[3]  
-digital4<-args[4]
-digital5<-args[5]
-digital6<-args[6]
-digital7<-args[7]
-digital8<-args[8]
-digital9<-args[9]
-digital10<-args[10]
-digital11<-args[11]
-digital12<-args[12]
-digital13<-args[13]
-digital14<-args[14]
-
-homofilia1<-args[15]
-homofilia2<-args[16] 
-homofilia3<-args[17]
-homofilia4<-args[18]
-homofilia5<-args[19]
-homofilia6<-args[20]
-homofilia7<-args[21]
-
 
 #Cambio de nombres en bdata
 
@@ -100,32 +81,26 @@ names(bdata$x) <-c("QID", "DigiCit","HomoIndex","Tr")
 
 args<-as.numeric(args)
 
-#### Funcion de agregar itemes
+#### Ciudadanía Digital
 
-DigitCount <-digital1+digital2+digital3+digital4+digital5+digital6+digital7+digital8+digital9+digital10+digital11
-+digital12+digital13+digital14
+DigitCount <-sum(args[2:15])
 
-DigiCit <-function(x){
-  if (DigitCount <= 56) {
-    0
-  } else {
-    1
-  }
-}
+bdata$orig$DigiCit <-ifelse(DigitCount<=56,"0",
+                            ifelse(DigitCount>=57,"1"))
+
+bdata$x$DigiCit <-ifelse(DigitCount<=56,"0",
+                         ifelse(DigitCount>=57,"1"))
 
 
-HomoIndex <-function(x){
-  if (HomoCount <= 39) {
-    0
-  } else {
-    1
-  }
-}
+# Homofilia política
 
+HomoCount <-sum(args[15:21])
 
-#producir un valor único por persona
+bdata$orig$HomoIndex <-ifelse(HomoCount<=39,"0",
+                              ifelse(HomoCount>=40,"1"))
 
-
+bdata$x$HomoIndex <-ifelse(HomoCount<=39,"0",
+                           ifelse(HomoCount>=40,"1"))
 
 
  #### Block Randomization
